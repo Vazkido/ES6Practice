ec20c1c20118fab37aa73a01e3e9727f94ff2ca2
valentine <vazkid02@gmail.com>
2025-01-28 14:09:10 -0500

    forEachfunction Es6

--- es.js
+++ es.js
@@ -1,5 +1,5 @@
 // This array is meant to find Jack inside variable people, if not, cannot find jack. Open this in life server.
-
+//START
 // const people = [{name: 'Max'}, {name: 'Jack'}, {name: 'Marry'}];
 // let i = 0;
 // while (i < people.length && people[i].name != 'Jack') {
@@ -10,21 +10,37 @@
 // } else {
 //     alert ('Cannot find Jack')
 // }
+//END
 
+//START
 /* THE BELOW ARRAY FUNCTION IS USED TO THE THE FIND FUNCTION BOTH IN JAVASCRIPT AND ES6 */
 
+// const people = [{ name: "Max" }, { name: "Jack" }, { name: "Marry" }];
+// //JavaScript
+// function findPerson(name) {
+//   for (let i = 0; i < people.length; i++) {
+//     let person = people[i];
+//     if (person.name == name) {
+//       return person;
+//     }
+//   }
+// }
+
+// //ES6
+// function findPerson(name) {
+// return people.find(person =>person.name == name)
+// }
+
+//END
+
+//START
 const people = [{ name: "Max" }, { name: "Jack" }, { name: "Marry" }];
-//JavaScript
-function findPerson(name) {
+// JavaScript
+function showEachOne(name) {
   for (let i = 0; i < people.length; i++) {
-    let person = people[i];
-    if (person.name == name) {
-      return person;
-    }
+    alert(people[i].name);
   }
 }
 
-//ES6
-function findPerson(name) {
-return people.find(person =>person.name == name)
-}
+// ES6
+const showEachOne = name => people.forEach(person => alert(person.name));
